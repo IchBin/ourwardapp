0d0e8f942766c426a42c680a80b4aaf2b1896737
Brad Grow <brad.grow@utah.edu>
2022-08-08 14:39:20 -0600

    Adjust properties for ward and api. Add hero-car ID for index.

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,39 +54,6 @@ function Home({ announcements, heroCard, faceCards, miniCards, imageCards }) {
       {dataAnnouncements.length > 0 && (
         <>
           <SectionHeader title="Announcements" subtitle="Find out more details of some of the upcoming events and activities." />
-          {/* <div className="pt-5">
-            <div className="bg-green-50 shadow-xl rounded-lg border-2 border-green-500">
-              <div className="px-4 py-5 sm:p-6">
-                <div className="sm:flex sm:items-start sm:justify-between">
-                  <div>
-                    <h3 className="text-lg leading-6 font-bold text-gray-900">Tithing Settlement</h3>
-                    <div className="mt-1 pr-2 flex items-center">
-                      <Icon name="calendar" className="h-4 w-4 mr-2 mb-0.5 text-gray-400" />
-                      <time>October 15th - December 31st</time>
-                    </div>
-                  </div>
-                  <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
-                    <PrimaryButton
-                      type="dark"
-                      color="green"
-                      link={{
-                        url: "https://calendly.com/ssr3-bishop/tithing",
-                        calendly: true,
-                      }}
-                    >
-                      Schedule
-                    </PrimaryButton>
-                  </div>
-                </div>
-                <div className="bg-white border border-gray-200 mt-3 text-base p-5 rounded-xl">
-                  <p>
-                    Please schedule a time for your family. Tithing settlement is an opportunity for each ward member to meet with the bishop, to make sure his
-                    or her donations records are correct, and to declare to the bishop his or her tithing status.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div> */}
           <div className="mt-7">
             <div className="relative max-w-xl mx-auto lg:max-w-7xl">
               <div className="grid gap-4 lg:grid-cols-2">
